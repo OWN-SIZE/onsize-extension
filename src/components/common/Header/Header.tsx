90552e17fadec79e3fa69bb1fc0bd43fbc59c498
leeseooo <leeseoooo@naver.com>
2023-01-02 18:55:41 +0900

    chore/#4: Styled 객체 추가

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -11,46 +11,45 @@ function Header(props: HeaderProps) {
   const { back, title, close } = props;
 
   return (
-    <Root>
-      <Back>{back && <img src={icBack} alt="back" />}</Back>
-      <Title>{title || null}</Title>
-      <Close>{close && <img src={icClose} alt="close" />}</Close>
-    </Root>
+    <Styled.Root>
+      <Styled.Back>{back && <img src={icBack} alt="back" />}</Styled.Back>
+      <Styled.Title>{title || null}</Styled.Title>
+      <Styled.Close>{close && <img src={icClose} alt="close" />}</Styled.Close>
+    </Styled.Root>
   );
 }
 
 export default Header;
 
-const Root = styled.div`
-  width: 100%;
-  height: 4.2rem;
-  display: flex;
-  padding: 1.8rem 1.7rem;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const Back = styled.div`
-  width: 2.4rem;
-  height: 2.4rem;
-  & > img {
+const Styled = {
+  Root: styled.div`
+    width: 100%;
+    height: 4.2rem;
+    display: flex;
+    padding: 1.8rem 1.7rem;
+    justify-content: space-between;
+    align-items: center;
+  `,
+  Back: styled.div`
     width: 2.4rem;
     height: 2.4rem;
-  }
-`;
-
-const Title = styled.h1`
-  font-weight: 600;
-  font-size: 1.8rem;
-  line-height: 2.5rem;
-  color: #000000;
-`;
-
-const Close = styled.div`
-  width: 2.4rem;
-  height: 2.4rem;
-  & > img {
+    & > img {
+      width: 2.4rem;
+      height: 2.4rem;
+    }
+  `,
+  Title: styled.h1`
+    font-weight: 600;
+    font-size: 1.8rem;
+    line-height: 2.5rem;
+    color: #000000;
+  `,
+  Close: styled.div`
     width: 2.4rem;
     height: 2.4rem;
-  }
-`;
+    & > img {
+      width: 2.4rem;
+      height: 2.4rem;
+    }
+  `,
+};
