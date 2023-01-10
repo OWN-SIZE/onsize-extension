0244575a151a14b42df30dc6ad5305eaa53f7267
Eunhyung Choi <milkiss0414@naver.com>
2023-01-10 14:40:31 +0900

    fix/#16: type input -> output

--- src/apis/inputSize.ts
+++ src/apis/inputSize.ts
@@ -1,8 +1,8 @@
-import { InputSizeInput } from '../types/inputSize';
+import { InputSizeInput, InputSizeOutput } from '../types/inputSize';
 
 import { client } from './index';
 
 export const postInputSize = async (inputSize: InputSizeInput) => {
-  const { data } = await client.post<InputSizeInput>(`/extension/inputSize`, inputSize);
+  const { data } = await client.post<InputSizeOutput>(`/extension/inputSize`, inputSize);
   return data;
 };
