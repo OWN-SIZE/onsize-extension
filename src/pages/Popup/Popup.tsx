907284c9a00ccc796e5dd173edc1ddadcb8a320b
leeseooo <leeseoooo@naver.com>
2023-01-12 01:31:59 +0900

    feat/#19: 상품 id 데이터 가공

--- src/pages/Popup/Popup.tsx
+++ src/pages/Popup/Popup.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useRecoilValue } from 'recoil';
 
 import { currentViewState } from '../../states/atom';
@@ -13,6 +14,24 @@ import SizeWrite from './size-write';
 
 function Popup() {
   const currentView = useRecoilValue(currentViewState);
+
+  const getUrl = async () => {
+    await chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const url = tabs[0].url;
+      if (!url) return;
+
+      const forwardUrlIndex = url.match('goods/')?.index;
+      const questionMarkIndex = url.split('').indexOf('?');
+
+      if (!forwardUrlIndex) return;
+      const productId = url?.slice(forwardUrlIndex + 6, questionMarkIndex);
+      console.log(productId);
+    });
+  };
+
+  useEffect(() => {
+    getUrl();
+  }, []);
 
   const renderView = () => {
     switch (currentView) {
