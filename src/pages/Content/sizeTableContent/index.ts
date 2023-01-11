00aac71c68a023b386082a6728af88fc56dd071d
leeseooo <leeseoooo@naver.com>
2023-01-12 02:42:12 +0900

    feat/#19: 사이즈표 key값 한글로 변경

--- src/pages/Content/sizeTableContent/index.ts
+++ src/pages/Content/sizeTableContent/index.ts
@@ -1,21 +1,32 @@
 import { InfoType, SizeInfoType } from '../../../types/content';
 
-const table = document.querySelector('table');
 const itemCategory = document.querySelector('.item_categories') as HTMLElement;
+let topOrBottom = ''; // 상의 또는 하의
+
+// 상하의 구분
+if (itemCategory) {
+  if (itemCategory.innerText.includes('바지')) {
+    topOrBottom = 'bottom';
+  } else {
+    topOrBottom = 'top';
+  }
+}
+
+const textMapper = {
+  총장: topOrBottom === 'top' ? 'topLength' : 'bottomLength',
+  어깨: 'shoulder',
+  가슴: 'chest',
+  허리: 'waist',
+  허벅지: 'thigh',
+  밑위: 'rise',
+  밑단: 'hem',
+};
+
+const table = document.querySelector('table');
 
 if (table) {
   const columns = table.querySelectorAll('.item_val') as NodeListOf<HTMLElement>;
   let sizeInfo: SizeInfoType = []; // 사이즈별 실측치 저장 배열
-  let topOrBottom = '';
-
-  // 상하의 구분
-  if (itemCategory) {
-    if (itemCategory.innerText.includes('바지')) {
-      topOrBottom = 'bottom';
-    } else {
-      topOrBottom = 'top';
-    }
-  }
 
   const tbody = table.querySelector('tbody');
   if (tbody) {
@@ -27,21 +38,23 @@ if (table) {
 
       // 실측 측정방식 객체로 저장
       [...columns].forEach((column) => {
-        /** TODO : key값을 영어로 변환 */
-        const key = column.innerText;
+        // 한글 key값 영어로 변환
+        const innerText = column.innerText.slice(0, column.innerText.indexOf('단면')) as keyof typeof textMapper;
+
+        const key = textMapper[innerText];
         if (!key) return;
         infoType[key] = 0;
       });
 
       const values = [...size.children].filter((element) => element.nodeName === 'TD') as HTMLElement[];
 
-      const th = size.children[0] as HTMLElement;
-      const MY = th.innerText; // 사이즈
-      infoType['size'] = MY;
-
       Object.keys(infoType).forEach((key, index) => {
         if (values[index]) infoType[key] = Number(values[index].innerText);
       });
+
+      const th = size.children[0] as HTMLElement;
+      const MY = th.innerText; // 사이즈
+      infoType['size'] = MY;
 
       sizeInfo = [...sizeInfo, infoType];
     });
