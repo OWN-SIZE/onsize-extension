2aed6568424222651ccddbb6a75f81bc3949c667
Eunhyung Choi <milkiss0414@naver.com>
2023-01-12 18:42:58 +0900

    fix/#20: api 코드 수정

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -6,6 +6,6 @@ export const BASE_URL = `${process.env.REACT_APP_SERVER}`;
 export const client = axios.create({
   baseURL: `${BASE_URL}/extension`,
   headers: {
-    Authorization: `Bearer ${token}`,
+    token: `${token}`,
   },
 });
--- src/apis/inputSize.ts
+++ src/apis/inputSize.ts
@@ -3,6 +3,6 @@ import { InputSizeInput, InputSizeOutput } from '../types/inputSize';
 import { client } from './index';
 
 export const postInputSize = async (inputSize: InputSizeInput) => {
-  const { data } = await client.post<InputSizeOutput>(`/extension/inputSize`, inputSize);
+  const { data } = await client.post<InputSizeOutput>(`/inputSize`, inputSize);
   return data;
 };
