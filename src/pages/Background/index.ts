dd0c586dac6e68e52d8ebd0dd0cf98eca774bac3
leeseooo <leeseoooo@naver.com>
2023-02-12 13:03:12 +0900

    feat/#35: tabs.onUpdated로 content script reload

--- src/pages/Background/index.ts
+++ src/pages/Background/index.ts
@@ -1,11 +1,3 @@
-let tabs;
-(async () => {
-  tabs = await chrome.tabs.query({ url: 'https://www.musinsa.com/*/goods/*' });
-  chrome.scripting.executeScript({ target: { tabId: tabs[0].id }, file: 'script/productContent.js' } as any);
-
-  tabs = await chrome.tabs.query({ url: 'https://www.mrporter.com/*/product/*' });
-  chrome.scripting.executeScript({ target: { tabId: tabs[0].id }, file: 'script/productContent.js' } as any);
-
-  tabs = await chrome.tabs.query({ url: 'https://www.ssense.com/*/product/*' });
-  chrome.scripting.executeScript({ target: { tabId: tabs[0].id }, file: 'script/productContent.js' } as any);
-})();
+chrome.tabs.onUpdated.addListener((tabs) => {
+  chrome.scripting.executeScript({ target: { tabId: tabs }, files: ['/script/productContent.js'] });
+});
