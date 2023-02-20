01e27a9cc95072f98900a91e7584a1139ab335e1
leeseooo <leeseoooo@naver.com>
2023-02-20 17:51:17 +0900

    feat/#41: background, content script reload 로직 수정

--- src/pages/Background/index.ts
+++ src/pages/Background/index.ts
@@ -1,3 +1,29 @@
-chrome.tabs.onUpdated.addListener((tabs) => {
-  chrome.scripting.executeScript({ target: { tabId: tabs }, files: ['/script/productContent.js'] });
-});
+const matchList = [
+  'https://www.musinsa.com',
+  'https://www.mrporter.com',
+  'https://www.ssense.com',
+  'https://www.okmall.com',
+  'https://product.29cm.co.kr',
+  'https://www.wconcept.co.kr',
+];
+
+function listener() {
+  chrome.tabs.onUpdated.addListener((tabs) => {
+    chrome.tabs.query({ active: true }, (tab) => {
+      const url = tab[0].url;
+      if (!url) return;
+      if (url.includes('ownsize.me')) {
+        chrome.scripting.executeScript({ target: { tabId: tabs }, files: ['/script/getUserData.js'] });
+        console.log('getUserData script reload');
+      }
+
+      if (matchList.some((v) => url.includes(v))) {
+        chrome.scripting.executeScript({ target: { tabId: tabs }, files: ['/script/productContent.js'] }).then(() => {
+          console.log('productContent script reload');
+        });
+      }
+    });
+  });
+}
+listener();
+chrome.tabs.onUpdated.removeListener(listener);
