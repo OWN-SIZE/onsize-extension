860f4866fd7d3b69630d765f2fcfb9187bffe4b7
leeseooo <leeseoooo@naver.com>
2023-02-24 22:45:05 +0900

    chore/#43: 쇼핑몰 URL 정규식 수정

--- src/pages/Background/index.ts
+++ src/pages/Background/index.ts
@@ -1,7 +1,7 @@
 const matchList = [
   'https://www.musinsa.com/[a-zA-Z]*/goods/[a-zA-Z]*',
-  'https://www.mrporter.com/[a-zA-Z]*/product/[a-zA-Z]*',
-  'https://www.ssense.com/[a-zA-Z]*/product/[a-zA-Z]*',
+  'https://www.mrporter.com/.*/[a-zA-Z]*/product/[a-zA-Z]*',
+  'https://www.ssense.com/.*/[a-zA-Z]*/product/[a-zA-Z]*',
   'https://www.okmall.com/products/[a-zA-Z]*',
   'https://product.29cm.co.kr/[a-zA-Z]*',
   'https://www.wconcept.co.kr/Product/[a-zA-Z]*',
